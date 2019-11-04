a618dd2b70ad744711ed00d39f1eb3055f65ad5f
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-11-04 17:12:44 +0100

    chore: package.json update, db update

--- src/db.js
+++ src/db.js
@@ -1,6 +1,19 @@
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
-const uri = 'mongodb://';
+const uri = 'mongodb://localhost:27017/db';
+const options = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-export const connect = () => mongoose.connect(uri);
+export const connect = () => {
+  mongoose.connect(uri, options)
+    .then(conn => {
+      console.log(`db connected on port #${conn.connections[0].port}`);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
